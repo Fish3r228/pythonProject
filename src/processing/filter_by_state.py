9716aad3aa4243ca21f0571619cfb877c0930d0f
Fish3r228 <zayarobert30@gmail.com>
2025-01-26 14:43:15 +0300

    новые функции

---
+++ src/processing/filter_by_state.py
@@ -0,0 +1,25 @@
+def filter_by_state(data, state='EXECUTED'):
+    """
+    Фильтрует список словарей по значению ключа 'state'.
+
+    data: Список словарей для фильтрации.
+    state: Значение ключа 'state', по которому производится фильтрация. По умолчанию 'EXECUTED'.
+    return: Новый список словарей, содержащий только те, у которых ключ 'state' соответствует указанному значению.
+    """
+    filtered_data = []
+    for item in data:
+        if item.get('state') == state:
+            filtered_data.append(item)
+    return filtered_data
+
+
+
+def sort_by_date(data, reverse=True):
+    """
+    Сортирует список словарей по дате.
+
+    data: Список словарей для сортировки.
+    reverse: Если True, сортировка по убыванию (по умолчанию). Если False, сортировка по возрастанию.
+    return: Новый список словарей, отсортированный по дате.
+    """
+    return sorted(data, key=lambda x: x.get('date', ''), reverse=reverse)
